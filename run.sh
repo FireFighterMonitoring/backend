203c83b929d6c6d666a59186249bc34dc79475ee
wujek srujek <wujek.srujek@gmail.com>
2016-02-17 15:07:54 +0100

    Fix stolen run.sh script

--- run.sh
+++ run.sh
@@ -5,20 +5,14 @@ set -e
 
 MY_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-MODULE_NAME="$(grep group "$MY_DIR"/gradle.properties | cut -d = -f 2).rest"
+MODULE_NAME="$(grep group "$MY_DIR"/gradle.properties | cut -d = -f 2).backend"
 CURRENT_VERSION="$(grep version "$MY_DIR"/gradle.properties | cut -d = -f 2)"
-APP_JAR="$MY_DIR/rest/build/libs/$MODULE_NAME-$CURRENT_VERSION.jar"
-LOCAL_CONFIG="$MY_DIR/application-local.properties"
+APP_JAR="$MY_DIR/build/libs/$MODULE_NAME-$CURRENT_VERSION.jar"
 
 if [ ! -f "$APP_JAR" ]; then
   echo "Cannot find '$APP_JAR' file - building"
-  "$MY_DIR/gradlew" -p "$MY_DIR" :rest:assemble
+  "$MY_DIR/gradlew" -p "$MY_DIR" clean assemble
   echo
 fi
 
-if [ ! -f "$LOCAL_CONFIG" ]; then
-  echo "Cannot find '$LOCAL_CONFIG' file - it is necessary to configure the required services."
-  exit 1
-fi
-
-java -jar "$APP_JAR" --spring.config.location="$LOCAL_CONFIG" "$@"
+java -jar "$APP_JAR" "$@"
