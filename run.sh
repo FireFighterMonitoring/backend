922fd8039a68ffd82e24b803927f29b164ac11d1
wujek srujek <wujek.srujek@gmail.com>
2016-02-19 22:06:04 +0100

    Fix root project name for gradle

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ set -e
 
 MY_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-MODULE_NAME="$(grep group "$MY_DIR"/gradle.properties | cut -d = -f 2).backend"
+MODULE_NAME="$(grep group "$MY_DIR"/gradle.properties | cut -d = -f 2).$(grep rootProject.name "$MY_DIR"/settings.gradle | cut -d \' -f 2)"
 CURRENT_VERSION="$(grep version "$MY_DIR"/gradle.properties | cut -d = -f 2)"
 APP_JAR="$MY_DIR/build/libs/$MODULE_NAME-$CURRENT_VERSION.jar"
 
